f3329ddb2438788c92d0e51480e94902fb177410
JErenci <joan.erencia@live.de>
2015-07-29 14:40:15 +0200

    1. Controller: Add HelloWorldController

---
+++ MVCTest/MVCTest/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTest.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+		//public ActionResult Index()
+		//{
+		//	return View();
+		//}
+        public String Index()
+        {
+			//return View();
+			return "This is my <b>default</b> action...";
+        }
+
+		//public string Welcome()
+		//{
+		//	return "This is the Welcome action method...";
+		//}
+
+		public string Welcome( string name, int ID = 1)
+		{
+			return HttpUtility.HtmlEncode( "Hello " + name + ", ID: " + ID );
+		}  
+
+    }
+}
